1c788a3e9cad8112088548a6e2fbdc65bf82c224
NYeonK <skdus728@gmail.com>
2023-01-28 00:22:17 +0900

    [ feat ] router 설정 및 연결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Router from "./Router";
 
 function App() {
   return (
     <div className="App">
+      <Router />
     </div>
   );
 }
