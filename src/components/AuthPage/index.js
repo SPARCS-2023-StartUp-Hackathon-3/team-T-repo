61b6dd5fe389f728207f72413f20b6d0fd2895f0
summit45 <brandy2145@gmail.com>
2023-01-28 15:28:56 +0900

    [ feat ] 구글 로그인/회원가입

--- src/components/AuthPage/index.js
+++ src/components/AuthPage/index.js
@@ -1,9 +1,62 @@
 import React, { useEffect, useState } from "react";
-import Login from "../Login";
-import { authService } from "../../fbase";
-import Nickname from "../NicknamePage";
+import { useNavigate } from "react-router-dom";
+import { authService, dbService, firebaseInstance } from "../../fbase";
+import AuthForm from "./AuthForm";
 
 export default function AuthPage() {
+  let navigate = useNavigate();
+  const [userData, setUserData] = useState([]);
+  const [userUid, SetUserUid] = useState("");
+
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    setUserData(data);
+
+    const userObj = {
+      uid: data.user.uid,
+    }
+
+    const userRef = dbService.collection('user').doc(`${data.user.uid}`);
+    const doc = await userRef.get();
+    if (!doc.exists) {  //첫 로그인이라면
+      console.log('No such document!');
+      navigate("/auth/nickname",{
+        replace: false,
+        state: {userObj: userObj}
+      })
+    } else {  //아니라면
+      console.log('Document data:', doc.data());
+      navigate("/",{
+        replace: false,
+        state: {userObj: userObj}
+      })
+    }
+
+    
+  };
+
+  return (
+    <div className="authContainer">
+      <AuthForm />
+      <div className="authBtns">
+        <button onClick={onSocialClick} name="google" className="authBtn">
+          Continue with Google
+        </button>
+      </div>
+    </div>
+  );
+
+
+
+
+  /*
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
@@ -23,6 +76,7 @@ export default function AuthPage() {
    
 <Login isLoggedIn={isLoggedIn} userObj={userObj}/>
   );
+  */
 }
 /*
  <>
--- src/components/NicknamePage/index.js
+++ src/components/NicknamePage/index.js
@@ -3,6 +3,60 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { dbService } from "../../fbase";
 
 export default function NicknamePage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [userId, setUserId] = useState(location.state.userObj.uid);
+  const [nickname, setNickName] = useState("");
+
+  const onChange = async (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNickName(value);
+  };
+
+  const onSubmit =  () => {
+    dbService.doc(`user/${userId}`).set({
+      nickname: nickname,
+      uid: userId,
+      data: Date.now(),
+    });
+
+    const userObj = {
+      uid: userId,
+    }
+
+    navigate("/",{
+      replace: false,
+      state: {userObj: userObj}
+    })
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              onChange={onChange}
+              type="text"
+              autoFocus
+              placeholder="닉네임"
+              value={nickname}
+            /> &nbsp;
+            <button>
+              닉네임 변경하기
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+/*
+export default function NicknamePage() {
   const location = useLocation();
   console.log('state', location.state)
   const { uid } = location.state;
@@ -25,7 +79,7 @@ export default function NicknamePage() {
             
           }
         }
-            /*
+            
             setChcek(!check)
             console.log(check);
             if(check){
@@ -36,7 +90,7 @@ export default function NicknamePage() {
 
           }
         }
-      });*/
+      });
     });
   });
   }, []);
@@ -108,3 +162,4 @@ export default function NicknamePage() {
     </>
   );
 };
+*/
