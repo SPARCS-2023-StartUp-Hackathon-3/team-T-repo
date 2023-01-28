5c58c6fd410d209f2a5aa9453f8517f3ef98299b
summit45 <brandy2145@gmail.com>
2023-01-28 10:14:55 +0900

    [ feat ] 팬 아트 카테고리 이미지, 제목 등 불러오기

--- src/components/CategoryPage/FanartPage/index.js
+++ src/components/CategoryPage/FanartPage/index.js
@@ -1,25 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "../../../fbase";
+import styled from "styled-components";
+import Item from "../item";
+
+const PostListWrapper = styled.div`
+  margin-top: 7px;
+  display: grid;
+  place-items: center;
+  justify-content: space-evenly;
+  /* font-size: small; */
+  /* row-gap: 3px; */
+  grid-template-columns: repeat(2, auto);
+`;
 
 export default function FanartPage() {
     const [postlists, setPostlists] = useState([]);
 
     useEffect(() => {
         dbService.collection("posts").where("category", "==", "fanart").orderBy("datetime", "desc")
-        .onSnapshot((snapshot) => {
-            const postArray = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            setPostlists(postArray);
-        });
-    },[]);
-
-    console.log(postlists)
+            .onSnapshot((snapshot) => {
+                const postArray = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setPostlists(postArray);
+            });
+    }, []);
 
     return (
         <>
             <div>FanartPage</div>
+            <PostListWrapper>
+                {postlists.map((list) => (
+                    <Item
+                        key={list.id}
+                        listObj={list}
+                        {...list}
+                    />
+                ))}
+            </PostListWrapper>
+
         </>
     )
 }
--- src/components/CategoryPage/index.js
+++ src/components/CategoryPage/index.js
@@ -4,9 +4,6 @@ import FanartPage from "./FanartPage";
 
 export default function CategoryPage() {
   const {categoryId} = useParams();
-  console.log(categoryId)
-
-  
 
   return (
     <>
