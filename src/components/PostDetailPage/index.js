f3aca4633ae195c317658d561dd0eb90719e7243
summit45 <brandy2145@gmail.com>
2023-01-28 10:15:05 +0900

    [ feat ] 게시글 불러오기

--- src/components/PostDetailPage/index.js
+++ src/components/PostDetailPage/index.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { dbService } from "../../fbase";
 
 export default function PostDetailPage() {
-  return <div>PostDetailPage</div>;
+  let navigate = useNavigate();
+  const {postId} = useParams();
+
+  const [detailObj, setDetailObj] = useState({});
+  
+  useEffect(() => {
+    dbService.collection("posts").doc(postId).get().then((doc) => {
+      const postObj = {
+        id: doc.id,
+        ...doc.data(),
+      };
+      setDetailObj(postObj);
+    });
+  })
+  
+  return(
+    <>
+     <div>PostDetailPage</div>;
+      <div>{detailObj.title}</div>
+      <div>{detailObj.content}</div>
+      <div>{detailObj.category}</div>
+      <div>{detailObj.aiModel}</div>
+      <div>{detailObj.prompt}</div>
+      <div>{detailObj.nonPrompt}</div>
+      {detailObj.attachmentUrl && (
+        <img src={detailObj.attachmentUrl} width="50%" height="50%" />
+      )}
+
+    
+    </>
+  )
+  
 }
