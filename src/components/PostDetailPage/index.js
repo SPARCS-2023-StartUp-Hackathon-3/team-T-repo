45b56bd9fe55a9155dc030bdd4c9820e4edf647c
Jueun Kim <92364973+Jueunkim429@users.noreply.github.com>
2023-01-29 10:57:42 +0900

    [ design ] 디테일 페이지 수정

--- src/components/PostDetailPage/index.js
+++ src/components/PostDetailPage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { dbService } from "../../fbase";
 import styled from "styled-components";
+import Header from "../common/Header"
 
 export default function PostDetailPage() {
   let navigate = useNavigate();
@@ -22,7 +23,7 @@ export default function PostDetailPage() {
   return (
     <>
       <>
-        {/* 상단배너*/}
+      <Header />
       </>
       <St.PostContainer>
         <St.ImageContainer>
@@ -37,10 +38,10 @@ export default function PostDetailPage() {
             {detailObj.content}
           </St.Content>
           <St.Category>
-            #{detailObj.category}
+            <b>#{detailObj.category}</b>
           </St.Category>
           <St.AiModel>
-            {detailObj.aiModel}
+            <b>Model: {detailObj.aiModel}</b>
           </St.AiModel>
           <St.Prompt>
             <b>Prompt</b><br></br>
@@ -67,12 +68,11 @@ const St = {
   PostContainer: styled.div`
   width: 65rem;
   height: 51.87rem;
-  margin: 10% auto 10% auto;
+  margin: 5% auto 10% auto;
   background-color: white;
   display: table;
   flex-direction: column;
   align-items: center;
-  border: 1px solid red;
   display: flex;
   flex-wrap: wrap;
   `
@@ -82,7 +82,6 @@ const St = {
   height: 51.87rem;
   background-color: black;
   float:left;
-  border: 1px solid blue;
   align-items: center;
   text-content: center;
   display: table-cell;
@@ -94,7 +93,6 @@ const St = {
   height: 49.87rem;
   background-color: white;
   margin: 1rem;
-  border: 1px solid black;
   background-image: url(${(props) => props.imgurl});
   background-size: cover;
   `
@@ -105,12 +103,10 @@ const St = {
   height: 51.87rem;
   margin-left: 1.63rem;
   background-color: white;
-  float:left;
-  border: 1px solid green;
   `
   ,
   Title: styled.div`
-  font-size: 2.5rem;
+  font-size: 2rem;
   font-weight: bold;
   margin-bottom: 1.5rem;
   margin-top: 1.5rem;
@@ -121,7 +117,7 @@ const St = {
   width: 16rem;
   height: 10.50rem;
   padding: 1.5rem;
-  font-size: 1.5rem;
+  font-size: 1.0rem;
   margin-bottom: 1.0rem;
   margin-left: 1.5rem;
   background-color: #d9d9d9;
