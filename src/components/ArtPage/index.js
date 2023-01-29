deb4a1e209d1af152a4051523a4101ada0d8fba3
NYeonK <skdus728@gmail.com>
2023-01-29 12:02:49 +0900

    [ feat ] slider 기능 구현

--- src/components/ArtPage/index.js
+++ src/components/ArtPage/index.js
@@ -1,14 +1,58 @@
 import React from "react";
 import Header from "../common/Header";
 import { St } from "./style";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./style.css";
 
 export default function ArtPage({ isLoggedIn, userObj }) {
+  const settings = {
+    className: "center",
+    //centerMode: true,
+    infinite: true,
+    //centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+    dots: true,
+    rows: 1,
+  };
   return (
     <>
       <Header isLoggedIn={isLoggedIn} userObj={userObj} />
       <St.TopContainer></St.TopContainer>
       <St.TopicContainer>Topic</St.TopicContainer>
-      <St.PostContainer>{/*여기에 추가*/}</St.PostContainer>
+      <St.PostContainer>
+        {" "}
+        <div className="SliderWrapper">
+          <Slider {...settings}>
+            <div>
+              <img src="img/Realistic.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/3D.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/Anime.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/Fanart.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/Fantasy.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/Gameart.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/Landscape.png" width="100%" />
+            </div>
+            <div className="box">
+              <img src="img/Painting.png" width="100%" />
+            </div>
+          </Slider>
+        </div>
+      </St.PostContainer>
       <St.TopicContainer>Today's Pick</St.TopicContainer>
       <St.TopContainer2></St.TopContainer2>
       <St.TopicContainer>명예의 전당</St.TopicContainer>
